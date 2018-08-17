5a72fba723d0e2cac10282f984d170369a4d5c0d
Lan Dam <ldam77@yahoo.com>
2018-08-17 10:35:04 -0700

    add correct day and date to title

--- src/app/title-bar/title-bar.component.ts
+++ src/app/title-bar/title-bar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-title-bar',
   templateUrl: './title-bar.component.html',
   styleUrls: ['./title-bar.component.css']
 })
-export class TitleBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class TitleBarComponent {
+  currentTime = new Date();
+  weekdays: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  dayOfWeek: string = this.weekdays[this.currentTime.getDay()];
+  month: string = this.months[this.currentTime.getMonth()];
+  date: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
 
 }
