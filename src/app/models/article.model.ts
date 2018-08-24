d40041d3b3a5b5750e87a8178f7545ab43e0841e
Lan Dam <ldam77@yahoo.com>
2018-08-24 14:33:03 -0700

    change model to include multiple tags

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -1,3 +1,3 @@
 export class Article {
-  constructor(public title:string, public author: string, public imageLink: string, public category: string, public subCategory: string, public body: string) { }
+  constructor(public title:string, public author: string, public imageLink: string, public category: string[], public body: string) { }
 }
