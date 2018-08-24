ece5e60902fe6f5e1428621dbf55006102782c4a
Lan Dam <ldam77@yahoo.com>
2018-08-24 09:09:34 -0700

    edit app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule }  from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { StoryBlockComponent } from './story-block/story-block.component';
 import { ImageBlockComponent } from './image-block/image-block.component';
+import { WelcomeComponent } from  './welcome/welcome.component';
+import { routing } from './app.routing';
 
 
 @NgModule({
